1ec36b4b17eed2ffd63652ca5aaa0668a977b046
Michal Vodicka <vodickam@gmail.com>
2023-12-05 21:18:51 +0100

    day 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,11 +1,53 @@
 import fs from 'fs'
+import readline from 'readline'
 
-import path from 'path'
+const numbers: Record<string,number> = {
+  "one": 1,
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9
+};
 
-const inputFile = path.resolve('./input.txt')
-const input = fs.readFileSync(inputFile, 'utf8')
+const matcher = (str: string) => {
+    let results = "";
+    for (let i = 0; i < str.length; i++) {
+      // check if number
+      if (Number(str[i])) {
+        results += Number(str[i]);
+      }
+      // there is no sense for checking strings shorter than 3
+      else if (str.length >= 3) {
+        Object.keys(numbers).forEach((nmbr) => {
+          if(str.substr(i).startsWith(nmbr)){
+                    results += numbers[nmbr];
+          }
+        });
+      }
+    }
+    return results;
+}
+//input.forEach((el) => el.matchAll(matcher));
+const readInterface = readline.createInterface({
+  input: fs.createReadStream('input.txt')
+});
 
-const lines = input.split(/\r?\n/)
+let input:string[] = []
+readInterface.on('line', function(line) {
+  input.push(line);
+});
 
-lines.map(console.log)
+readInterface.on('close', function() {
+  console.log("Result 1 task is", input
+    .map((line) => Array.from(line).filter(Number)).reduce((acc,curr)=> acc + Number('' + curr[0] + curr[curr.length-1]),0 )
+  )
+
+  console.log("Result 2 task is: ", input.map(matcher).reduce((acc,curr)=> acc + Number('' + curr[0] + curr[curr.length-1]),0 ));
+
+});
+
 
